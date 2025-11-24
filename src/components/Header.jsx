fe48a27330b63a7b3fa7955d9db5387581e9ae14
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2025-11-24 17:07:17 +0600

    theme customization, font & date-fns integration

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,20 @@
+import { format, isWithinInterval } from "date-fns";
 import logo from "../assets/logo.png";
 
 const Header = () => {
   return (
     <div className="flex justify-center flex-col items-center gap-3">
       <img className="w-[471px] " src={logo} />
-      <p>Journalism Without Fear or Favour</p>
+      <p className="text-accent text-[18px] leading-[30px] ">
+        Journalism Without Fear or Favour
+      </p>
+      <p className="text-[20px] font-medium text-accent ">
+        {" "}
+        <span className="text-primary">
+          {format(new Date(), "iiii,")}
+        </span>{" "}
+        {format(new Date(), "MMMM d, yyyy")}{" "}
+      </p>
     </div>
   );
 };
