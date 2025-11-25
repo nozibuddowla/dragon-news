bc2172d8da18ac2461662d1c4dd03f08da1f8aad
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2025-11-25 14:29:00 +0600

    load news data & filter news data based on category

--- src/Pages/CategoryNews.jsx
+++ src/Pages/CategoryNews.jsx
@@ -1,15 +1,38 @@
-import React from 'react';
-import { useParams } from 'react-router';
+import React, { useEffect, useState } from "react";
+import { useLoaderData, useParams } from "react-router";
 
 const CategoryNews = () => {
-    const {id} = useParams();
-    console.log(id);
-    
-    return (
-        <div>
-            <p>Category news - {id}</p>
-        </div>
-    );
+  const { id } = useParams();
+  const data = useLoaderData();
+  const [categoryNews, setCategoryNews] = useState([]);
+  const newsId = parseInt(id);
+
+  useEffect(() => {
+    if (newsId === 0) {
+      setCategoryNews(data);
+      return;
+    } else if (newsId === 1) {
+      const filterNews = data.filter(
+        (news) => news.others.is_today_pick === true
+      );
+        setCategoryNews(filterNews);
+    } else if (newsId === 10) {
+        const filterNews = data.filter(
+          (news) => news.others.is_trending === true
+        );
+        setCategoryNews(filterNews);
+    }
+    else {
+      const filterNews = data.filter((news) => news.category_id === newsId);
+      setCategoryNews(filterNews);
+    }
+  }, [data, id]);
+
+  return (
+    <div>
+      <p>Total - {categoryNews.length} news Found</p>
+    </div>
+  );
 };
 
 export default CategoryNews;
