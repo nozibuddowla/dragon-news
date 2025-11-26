c9e6f6e0b9793d2bd1f44062413b02f7c6c7575c
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2025-11-26 15:26:47 +0600

    add register user and OnAuthStateChange functionality

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { use } from "react";
 import { Link } from "react-router";
+import { AuthContext } from "../provider/AuthProvider";
 
 const Register = () => {
+  const { createUser, setUser } = use(AuthContext);
+  const handleRegister = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const photo = form.photo.value;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(name, photo, email, password);
+    createUser(email, password)
+      .then((result) => {
+        const user = result.user;
+        // console.log(user);
+        setUser(user)
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        alert(errorMessage);
+        
+      });
+  };
   return (
     <div className="flex justify-center items-center min-h-screen">
       <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl py-6">
@@ -9,29 +32,47 @@ const Register = () => {
           Register an account
         </h2>
         <hr className="my-4 text-base-300" />
-        <div className="card-body">
+        <form onSubmit={handleRegister} className="card-body">
           <fieldset className="fieldset">
             <label className="label">Your Name</label>
             <input
               type="text"
+              name="name"
               className="input"
               placeholder="Enter Your Name"
+              required
             />
 
             <label className="label">Photo URL </label>
             <input
               type="text"
+              name="photo"
               className="input"
               placeholder="Enter Your Photo"
+              required
             />
 
             <label className="label">Email</label>
-            <input type="email" className="input" placeholder="Email" />
+            <input
+              type="email"
+              name="email"
+              className="input"
+              placeholder="Email"
+              required
+            />
 
             <label className="label">Password</label>
-            <input type="password" className="input" placeholder="Password" />
-            
-            <button className="btn btn-neutral mt-4">Register</button>
+            <input
+              type="password"
+              name="password"
+              className="input"
+              placeholder="Password"
+              required
+            />
+
+            <button type="submit" className="btn btn-neutral mt-4">
+              Register
+            </button>
             <p className="font-semibold text-center pt-5">
               Already have an account?{" "}
               <Link className="text-secondary " to="/auth/login">
@@ -39,7 +80,7 @@ const Register = () => {
               </Link>{" "}
             </p>
           </fieldset>
-        </div>
+        </form>
       </div>
     </div>
   );
